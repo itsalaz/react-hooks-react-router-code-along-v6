80352b2dabf31ffc0bd4ee69b375dd2e9cdd411f
Elizabeth <itsalaz@icloud.com>
2024-07-01 17:56:21 -0400

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
 import Home from "./pages/Home";
+import About from "./pages/About"
+import Login from "./pages/Login"
+import UserProfile from "./pages/UserProfile"
+
+const router = createBrowserRouter(routes)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
